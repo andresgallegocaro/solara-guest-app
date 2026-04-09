e266f3aac29417c8b461faa4db984bbee36db92d
andresgallegocaro <andres.gallego.caro@gmail.com>
2026-04-09 05:03:58 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import GuestApp from './pages/GuestApp'
 import AdminPanel from './pages/AdminPanel'
 import OwnerPortal from './pages/OwnerPortal'
 import LoyaltySystem from './LoyaltySystem'
-
+import PricingEngine from './PricingEngine'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
@@ -16,6 +16,7 @@ createRoot(document.getElementById('root')).render(
         <Route path="/admin" element={<AdminPanel />} />
         <Route path="/owner" element={<OwnerPortal />} />
         <Route path="/guest/loyalty" element={<LoyaltySystem />} />
+        <Route path="/pricing" element={<PricingEngine />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
